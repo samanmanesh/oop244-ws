78f26a73bed53682658fceeaf57df151f074cbf5
Saman <samanmanesh7@gmail.com>
2021-09-17 09:19:08 -0700

    about to work on ws1-a

---
+++ ws1-a/ws1/drink.h
@@ -0,0 +1,36 @@
+// ===================================================================================
+//  Workshop: #1
+//  Part: 1 (Lab) 
+// ===================================================================================
+//  Student Name  : Mohammadhossein Sobhanmanesh
+//  Student ID    : 116523200
+//  Student Email : msobhanmanesh@myseneca.ca
+//  Course Section: OOP244-NAA
+// ===================================================================================
+
+#ifndef SDDS_IODRINK_H
+#define SDDS_IODRINK_H
+
+namespace sdds {
+
+	struct Drink {
+		char name[10];
+		int volume;
+		double price;
+		int quantity;
+	};
+
+	
+
+
+	void loadDrinks();
+	void drinkMenu();
+	void displayDrink(const Drink* drink);
+	int chooseDrink();
+	bool updateDrink(int sel);
+
+
+}
+
+#endif // 
+
