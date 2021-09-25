b1259c1b66bb77baf5320ca386e63e55ff1cd217
Saman <samanmanesh7@gmail.com>
2021-09-25 14:36:18 -0700

    finish the ws2-b just need to check on matrix

--- WS02/DIY/Guests.cpp
+++ WS02/DIY/Guests.cpp
@@ -1,3 +1,18 @@
+// ===================================================================================
+//  Workshop: #2
+//  Part: 2 (DIY)
+// ===================================================================================
+//  Student Name  : Mohammadhossein Sobhanmanesh
+//  Student ID    : 116523200
+//  Student Email : msobhanmanesh@myseneca.ca
+//  Date          : 2020-09-25
+//  Course Section: OOP244-NAA
+// ===================================================================================
+// I have done all the coding by myself and only copied the code that my professor
+// provided to complete my workshops and assignments.
+// ===================================================================================
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
 #include "Guests.h"
@@ -48,14 +63,33 @@ namespace sdds {
 
 	void print(const GuestList& theGuestList) {
 		int len = theGuestList.m_noOfGuests;
-			for (int i = 0; i < len; i++)
-			{
-				cout << i + 1 << "- ";
-				print(theGuestList.m_gst[i]);
-			}
+		for (int i = 0; i < len; i++)
+		{
+			cout << i + 1 << "- ";
+			print(theGuestList.m_gst[i]);
+		}
 	}
 
 
+	void addGuest(GuestList& theGuestList, const Guest& aGuest) {
+		int prevSize = theGuestList.m_noOfGuests;
+		int newSize = theGuestList.m_noOfGuests + 1;
+		Guest* newGuestPtr = nullptr;
+		newGuestPtr = new Guest[newSize];
+
+		for (int i = 0; i < prevSize; i++)
+		{
+			strcpy(newGuestPtr[i].m_name, theGuestList.m_gst[i].m_name);
+			newGuestPtr[i].m_phno = theGuestList.m_gst[i].m_phno;
+		}
+
+		strcpy(newGuestPtr[prevSize].m_name, aGuest.m_name);
+		newGuestPtr[prevSize].m_phno = aGuest.m_phno;
+		delete[] theGuestList.m_gst;
+		theGuestList.m_gst = newGuestPtr;
+		newGuestPtr = nullptr;
+		theGuestList.m_noOfGuests++;
+	}
 
 
 
--- WS02/DIY/Guests.h
+++ WS02/DIY/Guests.h
@@ -1,3 +1,17 @@
+// ===================================================================================
+//  Workshop: #2
+//  Part: 2 (DIY)
+// ===================================================================================
+//  Student Name  : Mohammadhossein Sobhanmanesh
+//  Student ID    : 116523200
+//  Student Email : msobhanmanesh@myseneca.ca
+//  Date          : 2020-09-25
+//  Course Section: OOP244-NAA
+// ===================================================================================
+// I have done all the coding by myself and only copied the code that my professor
+// provided to complete my workshops and assignments.
+// ===================================================================================
+
 #ifndef SDDS_GUESTS_H
 #define SDDS_GUESTS_H
 
@@ -29,6 +43,8 @@ namespace sdds {
    void print(const Guest& theGuest);
 
    void print(const GuestList& theGuestList);
+
+   void addGuest(GuestList& theGuestList, const Guest& aGuest);
 
 }
 
