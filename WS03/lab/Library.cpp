534b04ea348b1271beb79c33ed0101c59a869881
Saman <samanmanesh7@gmail.com>
2021-09-28 12:15:34 -0700

    set the modules and name sapces for them

---
+++ WS03/lab/Library.cpp
@@ -0,0 +1,28 @@
+
+// ===================================================================================
+//  Workshop: #3
+//  Part: 1 (Lab)
+// ===================================================================================
+//  Student Name  : Mohammadhossein Sobhanmanesh
+//  Student ID    : 116523200
+//  Student Email : msobhanmanesh@myseneca.ca
+//  Date          : 2020-09-29
+//  Course Section: OOP244-NAA
+// ===================================================================================
+// I have done all the coding by myself and only copied the code that my professor
+// provided to complete my workshops and assignments.
+// ===================================================================================
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include "Library.h"
+using namespace std;
+using namespace sdds;
+
+namespace sdds {
+
+
+
+}
---
+++ WS03/lab/Library.h
@@ -0,0 +1,26 @@
+// ===================================================================================
+//  Workshop: #3
+//  Part: 1 (Lab)
+// ===================================================================================
+//  Student Name  : Mohammadhossein Sobhanmanesh
+//  Student ID    : 116523200
+//  Student Email : msobhanmanesh@myseneca.ca
+//  Date          : 2020-09-29
+//  Course Section: OOP244-NAA
+// ===================================================================================
+// I have done all the coding by myself and only copied the code that my professor
+// provided to complete my workshops and assignments.
+// ===================================================================================
+
+#ifndef SDDS_LIBRARY_H
+#define SDDS_LIBRARY_H
+
+namespace sdds {
+
+
+
+
+
+}
+
+#endif
