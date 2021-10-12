ec510af8e5e5a5e11018e14aa69244c88d009bf1
Saman <samanmanesh7@gmail.com>
2021-10-12 15:20:13 -0700

    start working on lab

--- WS05/lab/EggCarton.cpp
+++ WS05/lab/EggCarton.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include "EggCarton.h"
 using namespace std;
+using namespace sdds;
+
+
+
 namespace sdds {
 
- 
+	EggCarton::EggCarton(int size = 6, int noOfEggs = 0, bool jumboSize = false) {
+		
+		if (size % 6 && size >= 6 && size <= 36 && noOfEggs >= 0 && noOfEggs <= size)
+		{
+			m_size = size;
+			m_noOfEggs = noOfEggs;
+			m_jumboSize = jumboSize;
+		}
+		else
+		{
+			setBroken();
+
+		}
+
+	};
+
+	void EggCarton::setBroken() {
+		m_size = -1;
+		m_noOfEggs = -1;
+	}
 
  
 }
--- WS05/lab/EggCarton.h
+++ WS05/lab/EggCarton.h
@@ -2,13 +2,27 @@
 #define SDDS_EGGCARTON_H
 #include <iostream>
 namespace sdds {
+
+	const int RegularEggWieght = 50;
+	const int JumboEggWieght = 75;
+
    class EggCarton {
+
+	   int m_size;
+	   int m_noOfEggs;
+	   bool m_jumboSize;
+
+	   void setBroken();
+
    public: 
+		
+	   EggCarton(int size = 6, int noOfEggs = 0, bool jumboSize = false);
+
+
+
 
 
    };
-
-
 
 }
 #endif
