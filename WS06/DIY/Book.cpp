8e0f1b3440c9d24f267733b6fdeebf036ee23076
Saman <samanmanesh7@gmail.com>
2021-10-24 11:59:25 -0700

    Started doing DIY of ws6

---
+++ WS06/DIY/Book.cpp
@@ -0,0 +1,25 @@
+// ===================================================================================
+//  Workshop: #6
+//  Part: 2 (DIY)
+// ===================================================================================
+//  Student Name  : Mohammadhossein Sobhanmanesh
+//  Student ID    : 116523200
+//  Student Email : msobhanmanesh@myseneca.ca
+//  Date          : 2020-10-24
+//  Course Section: OOP244-NAA
+// ===================================================================================
+// I have done all the coding by myself and only copied the code that my professor
+// provided to complete my workshops and assignments.
+// ===================================================================================
+
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include "Book.h"
+using namespace std;
+using namespace sdds;
+namespace sdds {
+
+	
+}
+
---
+++ WS06/DIY/Book.h
@@ -0,0 +1,36 @@
+// ===================================================================================
+//  Workshop: #6
+//  Part: 2 (DIY)
+// ===================================================================================
+//  Student Name  : Mohammadhossein Sobhanmanesh
+//  Student ID    : 116523200
+//  Student Email : msobhanmanesh@myseneca.ca
+//  Date          : 2020-10-24
+//  Course Section: OOP244-NAA
+// ===================================================================================
+// I have done all the coding by myself and only copied the code that my professor
+// provided to complete my workshops and assignments.
+// ===================================================================================
+
+#ifndef SDDS_BOOK_H
+#define SDDS_BOOK_H
+#include <iostream>
+namespace sdds {
+
+    const int MaxNameLength = 55;
+
+    class Book {
+        char* m_name{}; // sets m_name to nullptr before any constructor invocation 
+        int m_area;
+        int m_exchangeCode;
+        int m_number;
+
+       
+    public:
+
+       
+    };
+
+    
+}
+#endif // !SDDS_BOOK_H
