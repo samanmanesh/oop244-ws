0105ee12dd3e985fad7baeed271824a785c098f8
Saman <samanmanesh7@gmail.com>
2021-11-10 15:28:40 -0800

    working on VehicleBasic class/ add constructor/NewAddress

--- WS07/lab/VehicleBasic.cpp
+++ WS07/lab/VehicleBasic.cpp
@@ -10,11 +10,42 @@ Section:
 #include <iomanip>
 #include <cstring>
 #include "VehicleBasic.h"
+using namespace sdds;
+using namespace std;
 
 namespace sdds
 {
 	
+	VehicleBasic::VehicleBasic(const char* licensePlateNo, int builtYear) :m_currentAddress("Factory")
+	{
+		strcpy(m_licensePlateNo, licensePlateNo);
+		m_builtYear = builtYear;
+
+	}
 	
-	
-	
+	void VehicleBasic::NewAddress(const char* address) {
+
+		if (!strcmp(m_currentAddress, address)) {
+
+			cout << "|";
+			cout.setf(ios::right);
+			cout.width(8);
+			cout << m_licensePlateNo;
+			cout.unsetf(ios::right);
+			cout << "| " << "|";
+			cout.setf(ios::right);
+			cout.width(20);
+			cout << m_currentAddress;
+			cout.unsetf(ios::right);
+			cout << " ---> ";
+			cout.width(20);
+			cout.setf(ios::left);
+			cout << address << endl;
+			cout.unsetf(ios::left);
+		
+			//not sure if should remove the old address or not check if works both way
+			m_currentAddress[0] = '\0';
+			strcpy(m_currentAddress, address);
+		}
+	}
 }
--- WS07/lab/VehicleBasic.h
+++ WS07/lab/VehicleBasic.h
@@ -8,11 +8,35 @@ Section:
 #ifndef SDDS_VehicleBasic_H
 #define SDDS_VehicleBasic_H
 #include<iostream>
-
 namespace sdds
 {
+	class VehicleBasic
+	{
+		char m_licensePlateNo[9];
+		char m_currentAddress[21];
+		int m_builtYear;
+
+	public:
+		VehicleBasic(const char* licensePlateNo, int builtYear);
+
+		~VehicleBasic();
+
+		void NewAddress(const char* address);
 
 
+
+	};
+
+	
+	
+	
+
+
+
+
+	VehicleBasic::~VehicleBasic()
+	{
+	}
 
 }
 
