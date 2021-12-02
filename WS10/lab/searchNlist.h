32e1a33e8b0ce2a90a6b2722424f813f5c7f8afd
Saman <samanmanesh7@gmail.com>
2021-12-02 07:24:51 -0800

    Added required comments and ready to check and submit on matrix

--- WS10/lab/searchNlist.h
+++ WS10/lab/searchNlist.h
@@ -1,8 +1,16 @@
-//Name:
-//ID:
-//Email:
-// Date:
-//Section:
+// ===================================================================================
+//  Workshop: #10
+//  Part: 1 (Lab)
+// ===================================================================================
+//  Student Name  : Mohammadhossein Sobhanmanesh
+//  Student ID    : 116523200
+//  Student Email : msobhanmanesh@myseneca.ca
+//  Date          : 2021-12-2
+//  Course Section: OOP244-NAA
+// ===================================================================================
+// I have done all the coding by myself and only copied the code that my professor
+// provided to complete my workshops and assignments.
+// ===================================================================================
 
 #ifndef SDDS_SEARCHNLIST_H_
 #define SDDS_SEARCHNLIST_H_
@@ -10,7 +18,14 @@
 #include "Collection.h"
 namespace sdds {
 
-
+	/*
+	The check template requires:
+	1- two different types
+	2- a pointer of an array of template object
+	3- overloaded operator[] of type1 (Collection)
+	4- address of type2 for comparison
+	5- == operator
+	*/
 	template<typename type1, typename type2>
 	bool check(const type1* arrOfTmpObj, int index, const type2& keyValue) {
 		bool result = false;
@@ -21,17 +36,19 @@ namespace sdds {
 	}
 
 
-
+	/*
+	The search template requires:
+	1- two different types
+	2- a pointer of an array of template object
+	3- overloaded operator[] of type1 (Collection)
+	4- a reference of type1 for inserting and changing data
+	*/
 	template<typename type1, typename type2>
 	bool search(const type1* arrOfTmpObj, int sizeOfArr, const type2& keyValue, Collection<type1>& Cl) {
 		bool result = false;
-
-		//Cl.resize(0);
 		for (int i = 0; i < sizeOfArr; i++)
 		{
 			if (check(arrOfTmpObj, i, keyValue)) {
-				//arrOfTmpObj[i] == keyValue;
-				//Cl = arrOfTmpObj[i];
 				Cl.add(arrOfTmpObj[i]);
 				result = true;
 			}
@@ -39,7 +56,10 @@ namespace sdds {
 		return result;
 	}
 
-
+	/*
+	The listArrayElements template requires:
+	1- overloaded operator[] of type
+	*/
 	template <typename type>
 	void listArrayElements(const char* title, const type* arrayOfTemp, int numOfElm) {
 
@@ -51,16 +71,16 @@ namespace sdds {
 
 	}
 
+	/*
+	 The listArrayElements template requires:
+	 1- reference of two instance of the same compound type.
+
+	*/
+
 	template <typename type>
 	bool sizeCheck(const Collection<type>& leftC, const Collection<type>& rightC) {
 		return leftC.size() > rightC.size();
 	};
-
-
-
-
-
-
 }
 #endif // !SDDS_SEARCH_H_
 
