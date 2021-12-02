4802a5c54ea5c9086b60d8754e6f21802b5b40f4
Saman <samanmanesh7@gmail.com>
2021-12-01 16:02:25 -0800

    wrote the first coding for searchNlist.h/may need debugging

--- WS10/lab/searchNlist.h
+++ WS10/lab/searchNlist.h
@@ -10,17 +10,51 @@
 #include "Collection.h"
 namespace sdds {
 
+
+	template<typename type>
+	bool check(const Collection<type>* arrOfTmpObj, int index, const type* keyValue) {
+		bool result = false;
+
+		result = arrOfTmpObj[index] == keyValue ? true : false;
+
+		return result;
+	}
+
+
+
+	template<typename type>
+	bool search(const Collection<type>* arrOfTmpObj, int sizeOfArr, const type* keyValue, const Collection<type>& Cl) {
+		bool result = false;
+
+		for (int i = 0; i < sizeOfArr; i++)
+		{
+			arrOfTmpObj[i] == keyValue;
+			Cl = arrOfTmp[i];
+			result = true;
+		}
+
+		return result;
+	}
+
+
+	template <typename type>
+	void listArrayElements(const char* title, const type* arrayOfTemp, int numOfElm) {
+
+		std::cout << title << endl;
+		for (int i = 0; i < numOfElm; i++)
+		{
+			std::cout << i + 1 << ": " << arrayOfTemp[i] << endl;
+		}
+
+	}
+
 	template <typename type>
 	bool sizeCheck(const Collection<type>& leftC, const Collection<type>& rightC) {
 		return leftC.size() > rightC.size();
 	};
 
 
-	template <typename type>
-	void listArrayElements(const char* title, const type* arrayOfTemp, int numOfElm) {
 	
-	
-	}
 
 
 
